b0abdabcd156ee3ea40a1acc6c4364a15d111952
lakshay-tanwar <91410414+lakshay-tanwar@users.noreply.github.com>
2022-07-20 14:06:56 +0530

    Update index.js

---
+++ quora/login_page/index.js
@@ -0,0 +1,21 @@
+let card = document.querySelector(".card")
+const signupBtn = document.querySelector(".signup_btn")
+
+
+signupBtn.addEventListener("click" , () => {
+    card.style.display = "block"
+//     if(card.classList.style.display = "block"){
+//         card.classList.style.display = "none"}
+//     else{
+//         "
+
+//     }
+ })
+
+
+// chrome.runtime.onMessage.addEventListener(function(rq, sender, sendResponse){
+//     setTimeout(function() {
+//         sendResponse({status: true})
+//     }, 1);
+//     return true;
+// } )
